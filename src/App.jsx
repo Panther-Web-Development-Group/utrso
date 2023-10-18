b2ea6b21ef8662e21fd28215ddbd68c0cd12dba9
GSUGuide21 <71561603+GSUGuide21@users.noreply.github.com>
2023-10-18 02:01:33 -0400

    more content!

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
 import AppLayout from './components/AppLayout';
 import Section from "./components/Section";
+import OrganizationsList from "./components/OrganizationsList";
 import './App.css';
 
 function App() {
 	return (
 		<AppLayout>
-			<Section title="About Us">
+			<Section title="Mission" id="mission">
 				<p>
 					The United Technology Registered Student Organizations, also known as UTRSO, 
 					is a student organization that serves as a coalition for all student organizations 
 					related to technological advancements and innovations, including programming, gaming, 
-					cybersecurity, and more.
+					cybersecurity, and more. The purpose of the United Technology Registered Student Organizations 
+					is to provide connections for all technology-related student organizations to collaborate, 
+					communicate, and network with other organizations and their members.
+
+					The United Technology Registered Student Organizations shall serve as a liaison for 
+					inter-organizational problem-solving (and relations) and streamline the exchange of 
+					information.
 				</p>
+			</Section>
+			<Section title="Our Founder" id="founder"></Section>
+			<Section title="Organization" id="organizations">
+				<OrganizationsList />
 			</Section>
 		</AppLayout>
 	);
---
+++ src/components/OrganizationsList.jsx
@@ -0,0 +1,7 @@
+// import OrganizationItem from "./OrganizationItem";
+
+function OrganizationsList() {
+    return (<h3>Organization List</h3>)
+}
+
+export default OrganizationsList;
