d76fd9d13463059fb1aa31edd79188541937891b
GSUGuide21 <71561603+GSUGuide21@users.noreply.github.com>
2023-10-18 02:15:19 -0400

    testing the organization data

--- src/components/OrganizationsList.jsx
+++ src/components/OrganizationsList.jsx
@@ -1,7 +1,30 @@
+import { useState, useEffect } from "react";
 // import OrganizationItem from "./OrganizationItem";
 
 function OrganizationsList() {
-    return (<h3>Organization List</h3>)
+    const [data, setData] = useState({});
+
+    const fetchJSON = async () => {
+        try {
+            const d = await fetch("./data.json");
+            setData(d.json());
+        } catch {
+            setData({});
+        }
+    };
+
+    useEffect(() => {
+        fetchJSON();
+    }, []);
+
+    return (
+        <>
+            {JSON.stringify(data)}
+            <ul className="organization-list" id="organization-list">
+            </ul>
+        </>
+        
+    );
 }
 
 export default OrganizationsList;
