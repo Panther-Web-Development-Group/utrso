f80ba6874ce3e74164e9ab17d1e60c84aa34b5d5
GSUGuide21 <71561603+GSUGuide21@users.noreply.github.com>
2023-10-17 21:30:35 -0400

    updating the header

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import { useState } from 'react';
 import './App.css';
 
 function App() {
-	const [count, setCount] = useState(0);
+	const name = "UTRSO";
 
 	return (
 		<>
 			<header className="header" id="header">
-				<h1>{count}</h1>
+				<h1 className="logo">
+					<a href="#" className="logo-link" id="logo-link">
+						{name.split("")
+							.map(letter => <span className="logo-letter" key={`letter-${letter}`}>{letter}</span>)}
+					</a> 
+				</h1>
 				<div className="test" id="test">Hello World</div>
 			</header>
 		</>
